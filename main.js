4f33327cea7b3ef7e25ca71b87bd0f10a8ed4d94
vis-prime <vishals1030@gmail.com>
2023-05-19 00:27:08 +0530

    new demo

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import Router from "vanilla-router"
 import { version } from "./package.json"
 import ThicknessDemo from "./src/ThicknessDemo"
 import MaterialSwapDemo from "./src/MaterialSwapDemo"
+import EffectsPlayground from "./src/EffectsPlayground"
 
 const gui = new GUI({ title: "Demos: v" + version, closeFolders: true })
 if (window.innerWidth < window.innerHeight) {
@@ -22,6 +23,7 @@ const Demos = {
   Home: () => HomeDemo(),
   Thickness: ThicknessDemo,
   MaterialSwap: MaterialSwapDemo,
+  EffectsPlayground: EffectsPlayground,
 }
 
 gui.add(params, "homeButton").name("🔙Home")
